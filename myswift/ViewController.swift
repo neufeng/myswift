877cbe2578d0777afc5fec5abf1af4e54c415d56
Steven Cheung <steven@Stevens-MacBook-Air.local>
2014-09-22 17:49:33 +0800

    init

---
+++ myswift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myswift
+//
+//  Created by Steven Cheung on 9/22/14.
+//  Copyright (c) 2014 tx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
